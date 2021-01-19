7b28d4dc761109d2fd5d6a4697eee13273c469c9
GeorgeMikhailCoder <47564437+GeorgeMikhailCoder@users.noreply.github.com>
2021-01-19 09:14:24 +0300

    Связь через интернет
    Попытка связаться через интернет с использованием ip webcam на телефоне. Эта опция недоступна, только через серевер ipwebcam
    main: значимых изменений нет

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import argparse
 
 # источник видеопотока, номер подключённой к системе камеры или ссылка на удалённую
 # example:
-# cameraSource = 0
-# cameraSource = 'http://qwerty:12345@192.168.43.1:8080/video'
-cameraSource = 'http://qwerty:12345@192.168.43.1:8080/video' # работает!
-
+# cameraSource = 0 # работает, локальная камера
+# cameraSource = 'http://homecam:15243@192.168.43.1:8080/video' # работает, ip webcam, локальная сеть
+# cameraSource = 'http://homecam:15243@10.243.165.231:8080/video' # не работает,  ip webcam, интернет
+cameraSource = 0
 
 # лица, занимающие меньше X% по среднему арифметическому отношений ширины и высоты к экрану отсеиваются
 kMinFace = 0.1
@@ -36,44 +36,39 @@ video_capture = cv2.VideoCapture(cameraSource)
 
 face_locations = []
 while True:
-    ret = video_capture.open(cameraSource)
+    ret, frame = video_capture.read()
     if not ret:
-        print("Camera doesn't opened!")
+        print("Video doesn't accepted!")
         print(f"Address of webcam:  {cameraSource}")
+        break
     else:
-        ret, frame = video_capture.read()
-        if not ret:
-            print("Video doesn't accepted!")
-            print(f"Address of webcam:  {cameraSource}")
+        # ширина и высота экрана
+        camWidth = video_capture.get(3)
+        camHeight = video_capture.get(4)
+
+        small_frame = cv2.resize(frame, (0, 0), fx=kx, fy=ky)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        if face_locations:
+            (top, right, bottom, left) = face_locations[0]
+            print(f"k = {1 / 2 * ((bottom - top) / (camHeight * ky) + (right - left) / (camWidth * kx))}")
+
+        for (top, right, bottom, left) in face_locations:
+            if 1/2*((bottom - top)/(camHeight*ky) + (right - left)/(camWidth*kx)) > kMinFace:
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:
             break
-        else:
-            # ширина и высота экрана
-            camWidth = video_capture.get(3)
-            camHeight = video_capture.get(4)
-
-            small_frame = cv2.resize(frame, (0, 0), fx=kx, fy=ky)
-            rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            if face_locations:
-                (top, right, bottom, left) = face_locations[0]
-                print(f"k = {1 / 2 * ((bottom - top) / (camHeight * ky) + (right - left) / (camWidth * kx))}")
-
-            for (top, right, bottom, left) in face_locations:
-                if 1/2*((bottom - top)/(camHeight*ky) + (right - left)/(camWidth*kx)) > kMinFace:
-                    # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                    top *= 4
-                    right *= 4
-                    bottom *= 4
-                    left *= 4
-
-                    # Draw a box around the face
-                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            cv2.imshow('Video', frame)
-
-            if cv2.waitKey(1) & 0xFF == 27:
-                break
 
 video_capture.release()
 cv2.destroyAllWindows()
