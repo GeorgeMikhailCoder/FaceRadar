42982c25d75526a7a4611d4b2ccf3982fe086945
GeorgeMikhailCoder <george.mikhail.m@gmail.com>
2021-02-01 09:39:32 +0300

    Точка сохранения
    main: трекинг сделан

--- main.py
+++ main.py
@@ -6,18 +6,15 @@ import argparse
 from math import sqrt, pow
 from pandas import DataFrame
 from os import remove
-from time import sleep
+from time import sleep, time
 from icecream import ic
 from cv2 import CascadeClassifier
 from dlib import cnn_face_detection_model_v1
 from dlib import get_frontal_face_detector
-<<<<<<< Updated upstream
-
-=======
 from threading import Thread, Lock
 from queue import Queue
 from ExtendFunctions import *
->>>>>>> Stashed changes
+
 
 print("start")
 # источник видеопотока, номер подключённой к системе камеры или ссылка на удалённую
@@ -25,13 +22,13 @@ print("start")
 # cameraSource = 0 # работает, локальная камера
 # cameraSource = 'http://homecam:15243@192.168.43.1:8080/video' # работает, ip webcam, локальная сеть
 # cameraSource = "rtsp://op1:Qw123456@109.194.108.56:1554/ISAPI/Streaming/Channels/101" # первая камера
-cameraSource = "rtsp://op1:Qw123456@109.194.108.56:2554/ISAPI/Streaming/Channels/101" # вторая камера
-# cameraSource = 0
+# cameraSource = "rtsp://op1:Qw123456@109.194.108.56:2554/ISAPI/Streaming/Channels/101" # вторая камера
+cameraSource = 0
 
 # адрес назначения, для отправления найденных лиц
-# urlDist = "http://127.0.0.1:8000"
+urlDist = "http://127.0.0.1:8000"
 # urlDist = "https://enhod9mv9wlxpy8.m.pipedream.net" # мой адрес
-urlDist = "https://enazur7xr2301az.m.pipedream.net" # адрес Романа
+# urlDist = "https://enazur7xr2301az.m.pipedream.net" # адрес Романа
 
 
 # лица, занимающие меньше X% по среднему арифметическому отношений ширины и высоты к экрану отсеиваются
@@ -48,49 +45,6 @@ ky = 0.5
 # максимальное расстояние между центрами лиц, при котором они считаются одним. Измеряется в долях по отношению к наибольшей стороне прямоугольника лица.
 maxDistance = 0.9
 
-<<<<<<< Updated upstream
-def differ(oldFace, newFace) -> float:
-# коэффициент разницы положений лиц, отношение расстояний между центрами к самой длинной стороне
-    (top0, right0, bottom0, left0) = oldFace
-    (top1, right1, bottom1, left1) = newFace
-    amax = min([(abs(bottom0 - top0)),
-                (abs(bottom1 - top1)),
-                (abs(right0 - left0)),
-                (abs(right1 - left1))
-                ]) # минимальная сторона прямоугольника (меньшего лица)
-    center0 = [top0 + (bottom0 - top0)/2, left0 + (right0 - left0)/2]
-    center1 = [top1 + (bottom1 - top1)/2, left1 + (right1 - left1)/2]
-
-    centerDif = sqrt(
-        pow(center1[0] - center0[0], 2) +
-        pow(center1[1] - center0[1], 2)
-    )
-    return centerDif/amax
-
-def arrayImage2json(arrayImage):
-# перекодируем картинку в формат json
-    a = arrayImage
-    red = a[:, :, 0]
-    gre = a[:, :, 1]
-    blu = a[:, :, 2]
-
-    rpd = DataFrame(data=red)
-    gpd = DataFrame(data=gre)
-    bpd = DataFrame(data=blu)
-
-    rj = rpd.to_json()
-    gj = gpd.to_json()
-    bj = bpd.to_json()
-
-    data = {
-        "red": rj,
-        "green": gj,
-        "blue": bj
-    }
-    return data
-
-=======
->>>>>>> Stashed changes
 def upload(image, url):
 # отправляем картинку по указанному url
     # session = requests.Session()
@@ -117,22 +71,8 @@ def faceDetected(frame, newFace):
     print(f"k = {koefSmall(newFace, kx, ky, camWidth, camHeight)}")
     imageToSend = frame[top:bottom, left:right]
     cv2.imshow("new face detect!", imageToSend)
-<<<<<<< Updated upstream
-    upload(imageToSend, urlDist)
-
-def ltwh2trbl(ltwh):
-    (left, top, width, heigh) = ltwh
-    return (top, left + width, top + heigh, left)
-    # return ltwh
-
-def showImage(title, image):
-    img = cv2.resize(image, (640, 480))
-    cv2.imshow(title, img)
-
-=======
     # upload(imageToSend, urlDist)
     Thread(target=upload, args=(imageToSend, urlDist)).start()
-
 
 def tracingFacesSimple(cur_face_locations, last_face_locations):
         for newFace in cur_face_locations:
@@ -219,7 +159,7 @@ def threadProcess(argList, func, inArgs, prev):
     argList.put(res)
 
 start = time()
->>>>>>> Stashed changes
+
 # обработка консольных параметров, перезапись констант, если они были переданы
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -237,99 +177,44 @@ if __name__ == "__main__":
     kx = float(namespace.kx)
     ky = float(namespace.ky)
 
-if type(cameraSource) == str and cameraSource[0:4] == "rtsp": # for rtsp
-    video_capture = cv2.VideoCapture(cameraSource, cv2.CAP_FFMPEG)
-else:
-    video_capture = cv2.VideoCapture(cameraSource)
-
-camWidth = video_capture.get(3)
-camHeight = video_capture.get(4)
-print(f"camWidth = {camWidth}, camHeigh = {camHeight}")
-
-last_face_locations = []
-cur_face_locations = []
-
-kadrEmpty = 0
-frame = None
-faceCascade = CascadeClassifier('haarcascade_frontalface_default.xml')
-dnnFaceDetector = cnn_face_detection_model_v1("mmod_human_face_detector.dat")
-HOG_face_detect = get_frontal_face_detector()
-
-while True:
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-    ret, frame = video_capture.read()
-    if not ret:
-        print("Video doesn't accepted!")
-        print(f"Address of webcam:  {cameraSource}")
-        break
+    if type(cameraSource) == str and cameraSource[0:4] == "rtsp": # for rtsp
+        video_capture = cv2.VideoCapture(cameraSource, cv2.CAP_FFMPEG)
     else:
+        video_capture = cv2.VideoCapture(cameraSource)
 
-        #изменение размера, перевод картинки в формат rgb
-        cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        small_frame = cv2.resize(frame, (0, 0), fx=kx, fy=ky)
-        rgb_small_frame = small_frame
-        # frame = rgb_small_frame
-        # определение координат лиц (прямоугольников):
+    camWidth = video_capture.get(3)
+    camHeight = video_capture.get(4)
+    print(f"camWidth = {camWidth}, camHeigh = {camHeight}")
 
-        # через face_recognition
-        cur_face_locations = face_locations(rgb_small_frame)
+    last_face_locations = []
+    cur_face_locations = []
 
-        # через каскады Хаара
-        # gray = cv2.cvtColor(rgb_small_frame, cv2.COLOR_RGB2GRAY)
-        # cur_face_locations = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(int(kMinFace*camWidth), int(kMinFace*camHeight)), flags=cv2.CASCADE_SCALE_IMAGE)
-        # cur_face_locations = [(top, left + width, top + heigh, left) for (left, top, width, heigh) in cur_face_locations]
-
-        # через HOG
-        # gray = cv2.cvtColor(rgb_small_frame, cv2.COLOR_RGB2GRAY)
-        # cur_face_locations = HOG_face_detect(gray, 1)
-        # cur_face_locations = [(face.top(), face.right(), face.bottom(), face.left()) for face in cur_face_locations]
-
-        # через нейросеть dlib очень медленно
-        # gray = cv2.cvtColor(rgb_small_frame, cv2.COLOR_RGB2GRAY)
-        # cur_face_locations = dnnFaceDetector(gray, 1)
-        # cur_face_locations = [(face.rect.top(), face.rect.right(), face.rect.bottom(), face.rect.left())  for face in cur_face_locations]
+    kadrEmpty = 0
+    frame = None
+    faceCascade = CascadeClassifier('haarcascade_frontalface_default.xml')
+    dnnFaceDetector = cnn_face_detection_model_v1("mmod_human_face_detector.dat")
+    HOG_face_detect = get_frontal_face_detector()
 
 
-        # заполнение массивов (для первого запуска)
-        if len(cur_face_locations) == 0:
-            kadrEmpty += 1
-            if kadrEmpty>maxKadrEmpty:
-                last_face_locations = []
-                showImage('Video', frame)
-            continue
-        else:
-            kadrEmpty = 0
-
-        # отрисовка прямоугольников на экран
-        # сравнение размера прямоугольника с минимальным
-        for face in cur_face_locations:
-            if not isTooSmall(face):
-                frame = drawRect1(frame, face)
-            else:
-                cur_face_locations.remove(face)
-
-        # трекинг: поиск совпадений местонахождеий лиц а прошлом кадре
-        # если найдено повторение - прекратить поиск
-        # если нет ни одного повторения - вырезать и отправить лицо
-        for newFace in cur_face_locations:
-            for oldFace in last_face_locations:
-                if differ(oldFace, newFace) < maxDistance:
-                    break
-            else:
-                faceDetected(frame, newFace)
-
-        # текущий кадр становится прошлым, отрисовка окна видео
-        last_face_locations = cur_face_locations
-        showImage('Video', frame)
+    while True:
         if cv2.waitKey(1) & 0xFF == 27:
             break
+        ret, frame = video_capture.read()
+        if not ret:
+            print("Video doesn't accepted!")
+            print(f"Address of webcam:  {cameraSource}")
+            break
+        else:
+            frame, last_face_locations, kadrEmpty = detect((frame, last_face_locations, kadrEmpty))
+            showImage('Video', frame)
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
 
 
 
-# закрываем видеопоток и окна
-video_capture.release()
-cv2.destroyAllWindows()
+    # закрываем видеопоток и окна
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
 
