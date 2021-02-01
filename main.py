6655ecb3b58362030a931efcaaf4c9be5587cd65
GeorgeMikhailCoder <george.mikhail.m@gmail.com>
2021-02-01 09:44:13 +0300

    Update main.py
    main: восстановлено корректное сохранение файла фотографии

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def upload(image, url):
 # отправляем картинку по указанному url
     # session = requests.Session()
     # data = arrayImage2json(image)
-    name = "screen.jpg"
+    name = "screen"+time().__str__()+".jpg"
     cv2.imwrite(name, image)
     file = open(name, 'rb')
     try:
